ffc751bfff46448d9f12bed76b7d980f8643dbba
Rosemary Stanley <rosemary.reilman@gmail.com>
2016-09-15 12:03:14 -0400

    Rebuilds js to a simple prototype the game

---
+++ js/game.js
@@ -0,0 +1,71 @@
+function Game(selection) {
+  this.choices = [
+    'rock',
+    'paper',
+    'scissors',
+    'lizard',
+    'spock'
+  ];
+
+  this.settings = {
+    'rock' : {
+      'paper' : [ 'computer', 'covers' ],
+      'scissors' : [ 'user', 'crushes' ],
+      'lizard' :  [ 'user', 'crushes' ],
+      'spock' : [ 'computer', 'vaporizes' ],
+      'rock'  : [ 'tie' ],
+    },
+    'paper' : {
+      'rock' : [ 'user', 'covers' ],
+      'scissors' : [ 'computer', 'cut' ],
+      'lizard' :  [ 'computer', 'eats' ],
+      'spock' : [ 'user', 'disproves' ],
+      'paper'  : [ 'tie' ],
+    },
+    'scissors' : {
+      'paper' : [ 'user', 'cut' ],
+      'rock' : [ 'computer', 'crushes' ],
+      'lizard' :  [ 'user', 'decapitates' ],
+      'spock' : [ 'computer', 'smashes' ],
+      'scissors'  : [ 'tie' ],
+    },
+    'lizard' : {
+      'paper' : [ 'user', 'eats' ],
+      'scissors' : [ 'computer', 'decapitates' ],
+      'rock' :  [ 'computer', 'crushes' ],
+      'spock' : [ 'user', 'poisons' ],
+      'lizard'  : [ 'tie' ],
+    },
+    'spock' : {
+      'paper' : [ 'computer', 'disproves' ],
+      'scissors' : [ 'user', 'smashes' ],
+      'lizard' :  [ 'computer', 'poisons' ],
+      'rock' : [ 'user', 'vaporizes' ],
+      'spock'  : [ 'tie' ],
+    },
+  };
+
+
+  this.player = selection;
+  this.computer = this.choices[Math.floor(Math.random() * this.choices.length)];
+  this.results = this.calculate();
+  this.winner = this.results[0];
+  this.message = this.compare();
+
+}
+
+Game.prototype.compare = function() {
+  if (this.player !== this.computer) {
+    if (this.winner === 'user') {
+      return this.player + ' ' + this.results[1] + ' ' + this.computer + '!';
+    } else {
+      return this.computer + ' ' + this.results[1] + ' ' + this.computer + '!';
+    }
+  } else {
+    return "A tie!";
+  }
+};
+
+Game.prototype.calculate = function() {
+  return this.settings[this.player][this.computer];
+};
--- js/main.js
+++ js/main.js
@@ -1,42 +1,18 @@
-var rulesArray = {
-    'rock' : {
-        'paper' : [ 'lose', 'covers' ],
-        'scissors' : [ 'win', 'crushes' ],
-        'lizard' :  [ 'win', 'crushes' ],
-        'spock' : [ 'lose', 'vaporizes' ],
-    },
-    'paper' : {
-        'rock' : [ 'win', 'covers' ],
-        'scissors' : [ 'lose', 'cut' ],
-        'lizard' :  [ 'lose', 'eats' ],
-        'spock' : [ 'win', 'disproves' ],
-    },
-    'scissors' : {
-        'paper' : [ 'win', 'cut' ],
-        'rock' : [ 'lose', 'crushes' ],
-        'lizard' :  [ 'win', 'decapitates' ],
-        'spock' : [ 'lose', 'smashes' ],
-    },
-    'lizard' : {
-        'paper' : [ 'win', 'eats' ],
-        'scissors' : [ 'lose', 'decapitates' ],
-        'rock' :  [ 'lose', 'crushes' ],
-        'spock' : [ 'win', 'poisons' ],
-    },
-    'spock' : {
-        'paper' : [ 'lose', 'disproves' ],
-        'scissors' : [ 'win', 'smashes' ],
-        'lizard' :  [ 'lose', 'poisons' ],
-        'rock' : [ 'win', 'vaporizes' ],
-    },
-};
+$('.fa').click(function() {
+  var game = new Game($(this).attr('data-value')),
+      winner = game.winner,
+      updateClass = '.' + winner + '-score',
+      score = parseInt($(updateClass).text());
 
-function updateScores(theWinner) {
-  var updateClass = '.' + theWinner + '-score';
-  var score = parseInt($(updateClass).text());
   ++score;
 
-  if (theWinner === 'user') {
+  $('.fa').removeClass('active');
+  $('.results').empty();
+  $('.computer span, .user-choice span').removeClass();
+
+  $(this).addClass('active');
+
+  if (winner === 'user') {
    $('.active, .results, .user-choice').addClass('winner');
       $('.active, .results, .user-choice').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e) {
         $('.active, .results, .user-choice').removeClass('winner');
@@ -44,45 +20,5 @@ function updateScores(theWinner) {
   }
 
   $(updateClass).empty().append(score);
-}
-
-function compareChoices(choice1, choice2) {
-  if (choice1 === choice2) {
-    updateScores('tie');
-    return "A tie!";
-  } else {
-    var result = rulesArray[choice1][choice2];
-    if (result[0] === 'win') {
-      updateScores('user');
-      return choice1 + ' ' + result[1] + ' ' + choice2 + '!';
-    } else {
-      updateScores('computer');
-      return choice2 + ' ' + result[1] + ' ' + choice1 + '!';
-    }
-  }
-}
-
-function getRandom(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-
-function runGame(userChoice) {
-  var gameChoices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-  var computerChoice = getRandom(gameChoices);
-  $('.user-choice span').addClass('fa fa-hand-' + userChoice + '-o fa-5x user-fa');
-  $('.computer span').addClass('fa fa-hand-' + computerChoice + '-o fa-5x');
-  var results = compareChoices(userChoice, computerChoice);
-  $('.results').append('<h1>' + results + '</h1>');
-
-}
-$('.fa').click(function() {
-  $('.fa').each(function() {
-    if ($(this).hasClass('active')) {
-      $(this).removeClass('active')
-    }
-  });
-  $('.results').empty();
-  $('.computer span, .user-choice span').removeClass();
-  $(this).addClass('active');
-  runGame($(this).attr('data-value'));
+  $('.results').append('<h1>' + game.message + '</h1>');
 });
